4df8602107a11ac620fc3d1143a365d27b705a8a
Prithviraj <prithvirajmahamulkar100@gmail.com>
2021-05-20 01:11:46 +0530

    List generate

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,5 +1,18 @@
+class CatalogModel {
+  static final items = [
+    Item(
+        id: 1,
+        name: "GUCCI",
+        desc: "Mens Guilty Black Eau De Toilette For Him 50 ml",
+        price: 5400,
+        color: "#335055",
+        image:
+            "https://sslimages.shoppersstop.com/sys-master/images/ha7/h42/15023626289182/7913603_NoColour.jpg_1088Wx1632H")
+  ];
+}
+
 class Item {
-  final String id;
+  final int id;
   final String name;
   final String desc;
   final num price;
@@ -8,14 +21,3 @@ class Item {
 
   Item({this.id, this.name, this.desc, this.price, this.color, this.image});
 }
-
-final products = [
-  Item(
-      id: "001",
-      name: "GUCCI",
-      desc: "Mens Guilty Black Eau De Toilette For Him 50 ml",
-      price: 5400,
-      color: "#335055",
-      image:
-          "https://sslimages.shoppersstop.com/sys-master/images/ha7/h42/15023626289182/7913603_NoColour.jpg_1088Wx1632H")
-];
